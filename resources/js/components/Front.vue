a47f525b6399b96a108881dd71715677cba64f2c
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-02-23 23:35:16 +0600

    basic working vue template

---
+++ resources/js/components/Front.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <h1 class="my-4">Shop Catalog</h1>
+                <div class="list-group">
+                    <a class="list-group-item">
+                    category.name
+                    </a>
+                </div>
+
+            </div>
+            <div class="col-lg-9">
+                <div class="row mt-4">
+                    <div class="col-lg-4 col-md-6 mb-4" >
+                        <div class="card h-100">
+                            <a href="#">
+                                <img class="card-img-top" src="http://placehold.it/700x400" alt="">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="#"> product.name</a>
+                                </h4>
+                                <h5> product.price</h5>
+                                <p class="card-text">product.description</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
