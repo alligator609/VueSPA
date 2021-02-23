d11d2fb6e1b9f18c9788b8b0e2c3f1a8dfb6d5cf
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-02-23 16:22:04 +0600

    done with api

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+
+
+class CategoryController extends Controller
+{
+    public function index(){
+
+        return CategoryResource::collection(Category::all());
+
+    }
+}
